f73803312a3f92d07faf0271e3d11f84e6e925e7
dobedh <dobedh@gmail.com>
2021-01-21 21:10:55 +0900

    yotube clone coding-210121

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet"
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter)
+
+export default app;
