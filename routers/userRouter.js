87135f262460e10d14e59b0a90f9659f9940b246
dobedh <dobedh@gmail.com>
2021-01-22 22:12:02 +0900

    yotube clone coding-210122

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,14 @@
-import express from "express"
+import express from "express";
+import { changePassword, editProfile, userDetail, users } from "../controllers/userController";
+import routes from "../routes";
+
 
 const userRouter = express.Router();
 
+userRouter.get(routes.users, users)
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+
 export default userRouter
+
---
+++ routes.js
@@ -0,0 +1,39 @@
+
+// home
+const HOME = "/"
+const JOIN = "/join"
+const LOGIN = "/login"
+const LOGOUT = "/logout"
+const SEARCH = "/search"
+
+//user 
+const USERS = "/users"
+const USERS_DETAIL = "/:id"
+const EDIT_PROFILE = "/edit-profile"
+const CHANGE_PASSWORD = "/chage-password"
+
+//videos
+const VIDEOS = "/videos";
+const UPLOAD = "/upload";
+const VIDEO_DETAIL = "/:id"
+const DELETE_VIDEO = "/:id/delete";
+const EDIT_VIDEO = "/:id/edit";
+
+const routes = {
+    home : HOME,
+    join : JOIN,
+    login : LOGIN,
+    logout : LOGOUT,
+    search : SEARCH,
+    users : USERS,
+    userDetail : USERS_DETAIL,
+    editProfile : EDIT_PROFILE,
+    changePassword : CHANGE_PASSWORD,
+    videos : VIDEOS,
+    upload : UPLOAD,
+    videoDetail : VIDEO_DETAIL,
+    deleteVideo : DELETE_VIDEO,
+    editVideo : EDIT_VIDEO
+};
+
+export default routes;
