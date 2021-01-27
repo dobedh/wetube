66673e6c2beadc4c317835ad7cce0369ac1c10f9
dobedh <dobedh@gmail.com>
2021-01-27 23:40:21 +0900

    youtube clone coding -210127

--- init.js
+++ init.js
@@ -2,6 +2,6 @@ import app from "./app";
 
 const PORT = 4000;
 
-const handleListening = () => console.log(`Listening on https://localhost:${PORT}`)
+const handleListening = () => console.log(`✅ Listening on https://localhost:${PORT}`)
 
 app.listen(PORT, handleListening)
